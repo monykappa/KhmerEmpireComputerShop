c15f8bbd3615bc502603eb905ba333edddbff722
Kim Ouddommony <kimmony039@gmail.com>
2023-10-27 12:48:14 +0700

    models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,80 @@
 from django.contrib import admin
+from .models import Category, Product, ProductSpec, Stock, Order, OrderItem
 
-# Register your models here.
+class ProductSpecInline(admin.StackedInline):
+    model = ProductSpec
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductSpecInline]
+    
+    list_display = ('brand_name', 'model', 'year', 'price_with_dollar', 'category')
+    list_filter = ('year', 'category')
+
+    def price_with_dollar(self, obj):
+        return f"${obj.price}"
+    price_with_dollar.short_description = 'Price'
+
+
+
+class ProductSpecAdmin(admin.ModelAdmin):
+    list_display = ('product_brand_name', 'product_model', 'price_with_dollar', 'cpu', 'memory', 'storage', 'graphics_card', 'display', 'operating_system')
+
+    list_select_related = ('product',)  # Specify the related model to retrieve
+
+    def product_brand_name(self, obj):
+        if obj.product:
+            return obj.product.brand_name if obj.product.brand_name else "N/A"
+        return "N/A"  # Handle cases where there is no related product
+
+    product_brand_name.short_description = 'Brand Name'
+
+    def product_model(self, obj):
+        if obj.product:
+            return obj.product.model if obj.product.model else "N/A"
+        return "N/A"  # Handle cases where there is no related product
+
+    product_model.short_description = 'Product Model'
+
+    def price_with_dollar(self, obj):
+        return f"${obj.product.price}" if obj.product else ""
+    price_with_dollar.short_description = 'Price'
+
+
+class StockInline(admin.TabularInline):
+    model = Stock
+    extra = 1
+
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'price_per_unit_with_dollar', 'total_price_with_dollar')
+
+    def price_per_unit_with_dollar(self, obj):
+        return f"${obj.product.price}"  # Assuming product has a price field
+    price_per_unit_with_dollar.short_description = 'Price per Unit'
+
+    def total_price(self, obj):
+        return f"${obj.quantity * obj.product.price}" if obj.product else ""
+    total_price.short_description = 'Total Price'
+
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+    list_display = ('id', 'user', 'total_price')
+
+    def user_username(self, obj):
+        return obj.user.username
+
+    user_username.short_description = 'User'
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductSpec, ProductSpecAdmin)
+admin.site.register(Stock, StockAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
--- home/models.py
+++ home/models.py
@@ -1,68 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
+# Create a model for the Category of products (e.g., laptops, desktops).
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
+    brand_name = models.CharField(max_length=200, null=True, blank=True)
+    model = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='product_images/')
-    is_laptop = models.BooleanField(default=False)  # This field identifies laptops
-    specifications = models.OneToOneField('ProductSpec', on_delete=models.CASCADE, null=True, blank=True)
-    stock = models.OneToOneField('Stock', on_delete=models.CASCADE, related_name='product_stock')
+    year = models.CharField(max_length=4, choices=[(str(year), str(year)) for year in range(2015, 2056)], null=True, blank=True)
 
+    def __str__(self):
+        return self.brand_name
 
+# Define the ProductSpec model with a ForeignKey to Product
 class ProductSpec(models.Model):
-    brand = models.CharField(max_length=255)
-    processor = models.CharField(max_length=255)
-    ram = models.CharField(max_length=20)
-    storage = models.CharField(max_length=255)
-    display = models.CharField(max_length=255)
-    graphics_card = models.CharField(max_length=255)
-    operating_system = models.CharField(max_length=255)
-    battery_life = models.CharField(max_length=20)
-    ports_connectivity = models.TextField()
-    weight = models.CharField(max_length=20)
-    dimensions = models.CharField(max_length=255)
-    additional_features = models.TextField()
-    warranty = models.CharField(max_length=255)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    cpu = models.CharField(max_length=100, null=True, blank=True)
+    cpu_detail = models.CharField(max_length=200, null=True, blank=True)
+    memory = models.CharField(max_length=100, null=True, blank=True)
+    storage = models.CharField(max_length=100, null=True, blank=True)
+    graphics_card = models.CharField(max_length=100, null=True, blank=True)
+    display = models.CharField(max_length=100, null=True, blank=True)
+    operating_system = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        if self.product:
+            return f"Specifications for Product #{self.product.pk}"
+        else:
+            return "Unassociated Product Specification"
+        
+
+
 
 class Stock(models.Model):
-    product = models.OneToOneField(Product, on_delete=models.CASCADE, related_name='stock_product')
-    quantity = models.PositiveIntegerField()
-    last_updated = models.DateTimeField(auto_now=True)
-
-class Customer(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-
-class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    order_date = models.DateTimeField(auto_now_add=True)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)  # The initial stock quantity
+
+    def __str__(self):
+        return f"{self.product.brand_name} - {self.product.model} - {self.product.year}"
+
+    def price_per_unit(self):
+        return self.product.price if self.product else 0
+
+    def total_price(self):
+        return self.price_per_unit() * self.quantity
+
+    def price_per_unit_with_dollar(self):
+        return f"${self.price_per_unit()}"
+
+    def total_price_with_dollar(self):
+        return f"${self.total_price()}"
+    
+# Create a model for customer orders.
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    ordered_products = models.ManyToManyField(Product, through='OrderItem')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'Order #{self.id} - {self.customer_name}'
+
+# Create a model for the items in an order.
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    subtotal = models.DecimalField(max_digits=10, decimal_places=2)
 
-class EmployeeRole(models.Model):
-    name = models.CharField(max_length=255)
-
-class Employee(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    roles = models.ManyToManyField(EmployeeRole, through='EmployeeAssignment')
-
-class EmployeeAssignment(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    role = models.ForeignKey(EmployeeRole, on_delete=models.CASCADE)
-    assigned_date = models.DateField()
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name}'
 
