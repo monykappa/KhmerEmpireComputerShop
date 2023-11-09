d7d0ebb9271b0b4ae7bf0bd74ef9d0d9ce3eb5b7
Kim Ouddommony <kimmony039@gmail.com>
2023-11-09 21:35:20 +0700

    repsonsive product and product details page

--- KhmerEmpireComputerShop/settings.py
+++ KhmerEmpireComputerShop/settings.py
@@ -150,6 +150,7 @@ LOGOUT_REDIRECT_URL = 'userprofile:signin'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
